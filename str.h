2d41c7b2d10fa0d219d4666e6f55bffa029570b8
Ndongo Njie <ndongonjie17@gmail.com>
2024-02-22 19:08:25 -0500

    updated

--- str.h
+++ str.h
@@ -1,13 +1,21 @@
 #ifndef STRING_H
 #define STRING_H
 
-#include <stddef.h> // For size_t
+#include <stddef.h> 
 
-// Function declarations
-size_t Str_getLength(const char **pstr);
-void Str_copy(char *tobeCopied, const char *pstr);
+/*Handles the get length function*/
+size_t Str_getLength(const char *pstr);
+
+/*Handles the string copy function*/
+char Str_copy(char *tobeCopied, const char *pstr);
+
+/*Handles string concatenate function*/
 char Str_concat(char *tobeConcat, const char *pstr);
+
+/*Handles the string compare function*/
 int Str_compare(const char *charToCompare, const char *pstr);
-const char *Str_search(const char *chrToSearch, const char *otherChar);
+
+/*Handles string search function*/
+/*const char *Str_search(const char *chrToSearch, const char *otherChar);*/
 
 #endif 
