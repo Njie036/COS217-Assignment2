4aeb5b3e4d0b7438ef409bf0f9b8046fea4f90f0
Ndongo Njie <ndongonjie17@gmail.com>
2024-02-24 20:52:39 -0500

    updated

--- replace.c
+++ replace.c
@@ -42,7 +42,7 @@ static size_t replaceAndWrite(const char *pcLine,
                 pointer += fromLen; 
                 replacements++; 
             } else {
-                fprintf(stdout, "%s", *pointer);
+                fprintf(stdout, *pointer);
                 pointer++;
             }
         }
